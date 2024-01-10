816b35e81acf5b8d8e4c300e5764172ee43266e3
Abhishek Gupta <abhishek.gupta@optiphoenix.com>
2024-01-10 10:17:15 +0530

    Implemented Mongodb Connection

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './.env'
 })
 
-
 connectDB()
-// import express from 'express';
-
-// const app = express();
-
-// (async () => {
-//     try{
-//        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//        app.on('error', (error)=>{
-//         console.log('ERROR:', error);
-//         throw error;
-//        })
-//        app.listen(process.env.PORT, ()=>{
-//         console.log(`Your server is running on port ${process.env.PORT}`);
-//        })
-//     }catch(error){
-//         console.log('Error:', error);
-//         throw error;
-//     }
-// })()
+.then(()=>{
+    app.on('error', (error) => {
+        console.log(error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Your Server is running on ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log('MongoDB Connection Failed :', err)
+})
